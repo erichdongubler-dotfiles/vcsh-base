68a01349e838cf311b412de16b0b02a79c519d5c
Erich Gubler <erichdongubler@gmail.com>
2016-07-11 17:24:45 -0600

    Modify sublime template creation to have correct paths

--- .dotfiles-extensions/sublime.sh
+++ .dotfiles-extensions/sublime.sh
@@ -2,7 +2,7 @@ function subl_create_project() {
 	read -p "Enter the name of your project: " project_name
 	echo "Project name: \"$project_name\""
 	project_file=".sublime/$project_name.sublime-project"
-	TEMPLATE_FILE="$HOME/Scripts/templates/Project.sublime-project"
+	TEMPLATE_FILE="$HOME/.dotfiles-extensions/templates/Project.sublime-project"
 	echo "Project file: $project_file"
 	mkdir -p .sublime
 	sed "s/Project/$project_name/" $TEMPLATE_FILE > "$project_file"
