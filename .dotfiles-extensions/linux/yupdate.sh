4fee5090dc6928acff9bc214c5b6a0c946abca87
Erich Gubler <erichdongubler@ldschurch.org>
2016-09-03 17:58:23 -0600

    Use Reflector before a yaourt call for yupdate

--- .dotfiles-extensions/linux/yupdate.sh
+++ .dotfiles-extensions/linux/yupdate.sh
@@ -1,3 +1,4 @@
 function yupdate() {
-	yaourt -Syuaa --noconfirm 
+	sudo reflector --verbose --country 'United States' -l 200 -p https --sort rate --save /etc/pacman.d/mirrorlist
+	yaourt -Syuaa --noconfirm
 }
