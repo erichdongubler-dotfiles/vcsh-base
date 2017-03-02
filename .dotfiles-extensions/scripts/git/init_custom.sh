9ce64aef182a3a1c22d4fc661c44b27239956e22
Erich Gubler <erichdongubler@gmail.com>
2017-03-02 00:12:43 -0700

    Improve git custom init script

--- .dotfiles-extensions/scripts/git/init_custom.sh
+++ .dotfiles-extensions/scripts/git/init_custom.sh
@@ -2,27 +2,19 @@ git_init_custom () {
 	local TEMPLATES_DIRECTORY="$HOME/.dotfiles-extensions/templates/git"
 
 	if ! [ -d "$TEMPLATES_DIRECTORY" ]; then
-		echo "No templates directory found at \"$TEMPLATES_DIRECTORY\""
+		echo "ERROR: No templates directory found at \"$TEMPLATES_DIRECTORY\""
 		exit 1
 	fi
-	for template in "$@"; do
-		local template_directory="$TEMPLATES_DIRECTORY/$1"
-		if ! [ -d "$template_directory" ]; then
-			echo "ERROR: unable to find template directory \"$template_directory\""
-			continue
-		fi
-	done
-
-	function replace_project_template() {
-		find . \( -path "./.git" \) -prune -o -type f -exec sed -i "s/\$PROJECT/$project_name/g" '{}' \;
-	}
 
 	git init
 	read -p "Enter the name of your project: " project_name
 	for template in "$@"; do
 		local template_directory=`echo "$TEMPLATES_DIRECTORY/$template"| sed 's/\/\//\//'`
-		cp -r "$template_directory"/. .
-		replace_project_template
+		if [ -d "$template_directory" ]; then
+			cp -r "$template_directory"/. .
+		fi
+
+		find . \( -path "./.git" \) -prune -o -type f -exec sed -i "s/\$PROJECT/$project_name/g" '{}' \; # Replace $PROJECT placeholders in the template
 
 		local template_script="$template_directory.sh"
 		if [ -f "$template_script" ]; then
@@ -30,9 +22,6 @@ git_init_custom () {
 		fi
 	done
 	git init-commit
-
-	unset git_init_template
-	unset replace_project_template
 }
 
 git_init_custom "$@"
