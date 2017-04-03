1806d460fa6a21ffbb5a8a9acaa0a87e9676e0ec
Erich Gubler <erichdongubler@gmail.com>
2017-04-03 08:08:34 -0600

    Add l. alias for getting dotfiles

--- .dotfiles/scripts/.ls.sh
+++ .dotfiles/scripts/.ls.sh
@@ -5,12 +5,14 @@ case "$DOTFILES_OS" in
 		export CLICOLOR=1
 		alias ll='ls -l --time-style='"'+%d-%m-%Y %H:%M:%S'"
 		alias la='ls -A'
+		alias l.='ls .!(|.)'
 		;;
 	*)
 		eval $(dircolors -b)
 		alias ls='ls --color=auto'
 		alias ll='ls -l --color=auto --time-style='"'+%d-%m-%Y %H:%M:%S'"
 		alias la='ls --color=auto -A'
+		alias l.='ls --color=auto .!(|.)'
 		;;
 esac
 
