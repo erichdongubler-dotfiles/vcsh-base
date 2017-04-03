d73b155c75c75da44dd65dd7328e708b3f92adf0
Erich Gubler <erichdongubler@gmail.com>
2017-04-03 08:08:04 -0600

    fzf: Don't use height option in Windows, as it's currently unimplemented

--- .dotfiles/scripts/.fzf.sh
+++ .dotfiles/scripts/.fzf.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-export FZF_DEFAULT_OPTS='--height 40% --reverse --multi'
+case "$DOTFILES_OS" in
+	"msys") ;&
+	"cygwin")
+		export FZF_DEFAULT_OPTS='--reverse'
+		;;
+	*)
+		export FZF_DEFAULT_OPTS='--height 40% --reverse'
+		;;
+esac
 
