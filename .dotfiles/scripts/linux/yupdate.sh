498d88981011232059feeaa482869cbb8d1c397b
Erich Gubler <erichdongubler@gmail.com>
2017-05-14 17:10:19 -0600

    Add cache-disabled mode for Raspberry Pis with yupdate

--- .dotfiles/scripts/linux/yupdate.sh
+++ .dotfiles/scripts/linux/yupdate.sh
@@ -1,5 +1,17 @@
-function yupdate() {
-	sudo reflector --verbose --country 'United States' -l 200 -p https --sort rate --save /etc/pacman.d/mirrorlist
+function yupdate_base() {
 	yaourt -Syuaac --devel --noconfirm && sudo pacman -Rsn $(sudo pacman -Qdtq)
 }
+
+function yupdate() {
+	sudo reflector --verbose --country 'United States' -l 200 -p https --sort rate --save /etc/pacman.d/mirrorlist
+	yupdate_base
+}
+
+if [ "$(uname -m | grep arm)" ]; then
+	echo "Warning: yupdate configured to clean cache"
+	function yupdate() {
+		yaourt -Scc
+		yupdate_base
+	}
+fi
 
