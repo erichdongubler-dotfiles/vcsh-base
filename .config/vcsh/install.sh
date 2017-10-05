d511cc867c9bbedd565d9735248bc80d06c72063
Erich Gubler <erichdongubler@gmail.com>
2017-10-05 10:47:21 -0600

    Use .local instead of .installed, since there's no reason to violate convention yet

--- .config/vcsh/install.sh
+++ .config/vcsh/install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-_INSTALL="$HOME/.installed"
+_INSTALL="$HOME/.local"
 _INSTALL_BIN="$_INSTALL/bin"
 mkdir "$_INSTALL" && mkdir "$_INSTALL_BIN"
 pushd "$_INSTALL_BIN" && export PATH="$PATH:$_INSTALL_BIN" && curl -O "https://raw.githubusercontent.com/RichiH/vcsh/master/vcsh" && cd && vcsh --help
@@ -8,5 +8,4 @@ popd
 
 vcsh clone git@github.com:erichdongubler-dotfiles/base.git
 
-echo 'Sourcing ~/.bash_profile...'
-. ~/.bash_profile
+echo 'Done! If you're in bash, the run \`. ~/.bash_profile\`.'
---
+++ .dotfiles/scripts/.local.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+LOCAL_BIN_PATH="$HOME/.local/bin"
+mkdir -p "$LOCAL_BIN_PATH"
+export PATH="$PATH:$LOCAL_BIN_PATH"
+
