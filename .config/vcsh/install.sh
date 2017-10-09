b5e4320f9186396af2f77811d7245bf0b163cdce
Erich Gubler <erichdongubler@gmail.com>
2017-10-08 20:41:57 -0600

    Fix up stuff after a test install on Linux

--- .config/vcsh/install.sh
+++ .config/vcsh/install.sh
@@ -2,10 +2,10 @@
 
 _INSTALL="$HOME/.local"
 _INSTALL_BIN="$_INSTALL/bin"
-mkdir "$_INSTALL" && mkdir "$_INSTALL_BIN"
-pushd "$_INSTALL_BIN" && export PATH="$PATH:$_INSTALL_BIN" && curl -O "https://raw.githubusercontent.com/RichiH/vcsh/master/vcsh" && cd && vcsh --help
+mkdir -p "$_INSTALL_BIN"
+pushd "$_INSTALL_BIN" && export PATH="$PATH:$_INSTALL_BIN" && curl -O "https://raw.githubusercontent.com/RichiH/vcsh/master/vcsh" && chmod +x "$_INSTALL_BIN/vcsh"
 popd
 
 vcsh clone git@github.com:erichdongubler-dotfiles/base.git
 
-echo 'Done! If you're in bash, the run \`. ~/.bash_profile\`.'
+echo "Done! If you're in bash, the run \`. ~/.bash_profile\`."
