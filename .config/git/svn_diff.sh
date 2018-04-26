740fb26c90119874b83122b5aa79d6c7e4e08e34
Erich Gubler <erichdongubler@gmail.com>
2018-04-26 09:28:26 -0600

    git: Move  script into its own file

---
+++ .config/git/svn_diff.sh
@@ -0,0 +1,34 @@
+ git diff --no-prefix "$@" | awk '
+
+/^diff --git/ {
+	print "Index: " $3
+	current_file=$3
+	next
+}
+
+/^index/ {
+	print "==================================================================="
+	next
+}
+
+/^--- [^/]/ {
+	cmd = "git svn find-rev $(git branch -r --merged HEAD)"
+	cmd | getline latest_revision
+	printf ("%s\t(revision %s)\n", $0, latest_revision)
+	next
+}
+
+/^\\+\\+\\+/ {
+	printf("%s\t(working copy)\n", $0)
+	next
+}
+
+/^--- [/]/ {
+	printf("--- %s\t(revision 0)\n", current_file)
+	next
+}
+
+{
+	print
+}
+ '
