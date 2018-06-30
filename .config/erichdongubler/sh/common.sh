03e87f01770c6c0b46863961d77371612e1c898d
Erich Gubler <erichdongubler@gmail.com>
2018-06-30 07:20:56 -0600

    Experiment: keep quotes away from wildcard globs in common.sh

--- .config/erichdongubler/sh/common.sh
+++ .config/erichdongubler/sh/common.sh
@@ -10,12 +10,12 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 		local scripts_directory="$XDG_CONFIG_HOME/erichdongubler/$shell_type/$script_type"
 		if [ -d "$scripts_directory" ]; then
 			echo -e "    from \033[1;34m\"$scripts_directory\"\033[00m"
-			for file in $(ls "$scripts_directory/."*".$shell_type" 2> /dev/null); do
+			for file in $(ls "$scripts_directory"/.*."$shell_type" 2> /dev/null); do
 				if [ -f "$file" ]; then
 					. "$file"
 				fi
 			done
-			for file in $(ls "$scripts_directory/"*".$shell_type"  2> /dev/null); do
+			for file in $(ls "$scripts_directory"/*."$shell_type" 2> /dev/null); do
 				if [ -f "$file" ]; then
 					echo -e "      Loading \033[1;34m${file##*/}\033[00m"
 					. "$file"
