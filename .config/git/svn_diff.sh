7b270bb6d1ec38541596dc7dda4fc4a28386d5e7
Erich Gubler <erichdongubler@gmail.com>
2018-08-08 15:34:25 -0600

    svn_diff.sh: whitespace fixes

--- .config/git/svn_diff.sh
+++ .config/git/svn_diff.sh
@@ -18,7 +18,7 @@
 /^--- [^/]/ {
 	cmd = "git svn find-rev $(git branch -r --merged HEAD)"
 	cmd | getline latest_revision
-	printf ("%s\t(revision %s)\n", $0, latest_revision)
+	printf("%s\t(revision %s)\n", $0, latest_revision)
 	next
 }
 
@@ -35,4 +35,4 @@
 {
 	print
 }
- '
+'
