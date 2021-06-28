8325912812053a5b124fa2bb70a47c543e2ec3ee
Erich Gubler <erichdongubler@gmail.com>
2021-06-28 10:17:26 -0600

    fix(vcsh): s/master/main/ for `install.sh`

--- .config/vcsh/install.sh
+++ .config/vcsh/install.sh
@@ -3,7 +3,7 @@
 _INSTALL="$HOME/.local"
 _INSTALL_BIN="$_INSTALL/bin"
 mkdir -p "$_INSTALL_BIN"
-pushd "$_INSTALL_BIN" && export PATH="$PATH:$_INSTALL_BIN" && curl -O "https://raw.githubusercontent.com/RichiH/vcsh/master/vcsh" && chmod +x "$_INSTALL_BIN/vcsh"
+pushd "$_INSTALL_BIN" && export PATH="$PATH:$_INSTALL_BIN" && curl -O "https://raw.githubusercontent.com/RichiH/vcsh/main/vcsh" && chmod +x "$_INSTALL_BIN/vcsh"
 popd
 
 vcsh clone git@github.com:erichdongubler-dotfiles/base.git
